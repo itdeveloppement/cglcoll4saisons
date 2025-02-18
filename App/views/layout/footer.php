672b5465ddc0e7a9f9678c53c43604ac2fde2b31
Marti <118295021+itdeveloppement@users.noreply.github.com>
2025-02-18 22:00:21 +0100

    correction footer presentation et lien

--- App/views/layout/footer.php
+++ App/views/layout/footer.php
@@ -6,33 +6,34 @@ Template de page : pied de page
             <footer class = "footer flex">
 
                 <?php
-                if (isset($langs) && is_object($langs)) {
-                    if ($langs->trans("text-footer")!= null) {
-                        echo '<h3>' . html_entity_decode(htmlspecialchars($langs->trans("text-footer"), ENT_QUOTES, 'UTF-8')) . '</h3>';
-                    } else {
-                        echo '';
-                    }
-                }
+                // if (isset($langs) && is_object($langs)) {
+                //     if ($langs->trans("text-footer")!= null) {
+                //         echo '<h3>' . html_entity_decode(htmlspecialchars($langs->trans("text-footer"), ENT_QUOTES, 'UTF-8')) . '</h3>';
+                //     } else {
+                //         echo '';
+                //     }
+                // }
                 ?>
-                <div class = "flex container-texte">  
-                    <p>
-                        <?php
-                        $telephone = "(+33) - 09 80 36 37 84";
-                        echo '<a href="tel:' . $telephone . '">' . $telephone . '</a>';
-                        ?>
-                    </p>      
-                    <?php
-                        $email = "contact@cigaleaventure.com";
-                        echo '<a href="mailto:' . $email . '">' . $email . '</a>';
-                        ?>
-                    </p>
-                    <p>
-                        <?php
-                        $url = "https://www.cigaleaventure.com";
-                        echo '<a href="' . $url . '" target="_blank">Visiter le site</a>';
-                        ?>
-                    </p>
-                    <p>Cigale Aventure, 3 rue de l'horloge 30120 Le Vigan </p>
+                <div class = "flex container-texte">
+                        <p class = "footer-p">
+                            <?php
+                            $telephone = "+330980363784";
+                            echo '<a href="tel:' . $telephone . '">(+33) 09 80 36 37 84</a>';
+                            ?>
+                        </p>
+                        <p class = "footer-p">
+                            <?php
+                            $email = "contact@cigaleaventure.com";
+                            echo '<a href="mailto:' . $email . '">contact@cigaleaventure.com</a>';
+                            ?>
+                        </p>
+                        <p class = "footer-p">
+                            <?php
+                            $url = "https://www.cigaleaventure.com";
+                            echo '<a href="' . $url . '" target="_blank">Web site</a>';
+                            ?>
+                        </p>
+                        <p class = "footer-p"><a href="https://www.google.com/maps?q=Cigale+Aventure,+3+rue+de+l'horloge+30120+Le+Vigan" target="_blank">Maps</a></p>
                 </div>
             </footer>
         <!-- div container -->
