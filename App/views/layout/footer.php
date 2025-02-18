f2cda784c2b7a9d5f3055d73e19cc2161758f44b
Marti <118295021+itdeveloppement@users.noreply.github.com>
2025-02-18 18:51:42 +0100

    link tel mail web site

--- App/views/layout/footer.php
+++ App/views/layout/footer.php
@@ -15,9 +15,24 @@ Template de page : pied de page
                 }
                 ?>
                 <div class = "flex container-texte">  
-                    <p>(+33) - 09 80 36 37 84</p>
-                    <p>contact@cigaleaventure.com</p>
-                    <p>3 rue de l'horloge 30120 Le Vigan </p>
+                    <p>
+                        <?php
+                        $telephone = "(+33) - 09 80 36 37 84";
+                        echo '<a href="tel:' . $telephone . '">' . $telephone . '</a>';
+                        ?>
+                    </p>      
+                    <?php
+                        $email = "contact@cigaleaventure.com";
+                        echo '<a href="mailto:' . $email . '">' . $email . '</a>';
+                        ?>
+                    </p>
+                    <p>
+                        <?php
+                        $url = "https://www.cigaleaventure.com";
+                        echo '<a href="' . $url . '" target="_blank">Visiter le site</a>';
+                        ?>
+                    </p>
+                    <p>Cigale Aventure, 3 rue de l'horloge 30120 Le Vigan </p>
                 </div>
             </footer>
         <!-- div container -->
