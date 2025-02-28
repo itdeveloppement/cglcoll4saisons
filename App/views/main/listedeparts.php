984917305d337de286fd2e6e2a7373fc46586e24
Marti <118295021+itdeveloppement@users.noreply.github.com>
2025-02-28 15:25:29 +0100

    Dev (#34)
    * removing the recording validation page (#23)
    
    * 25 bug   form   phrase tier (#26)
    
    * change css and sentence tier
    
    * change css and sentence tier
    
    * 27 bug   form   button (#30)
    
    * desable the display LO
    
    * change size bittun fist page
    
    * change in the display order of activities
    
    * change erreur and confirm message
    
    * 31 bug   form   css divers (#32)
    
    * fix button validate information
    
    * fix button validate information
    
    * fix button validate information
    
    * font color placeholder : raid
    
    * error in merged code review

--- App/views/main/listedeparts.php
+++ App/views/main/listedeparts.php
@@ -32,16 +32,8 @@ echo '<main class="liste-depart">';
                     // echo "<p>" . $depart->get("lieuDepart") . "</p>";
                     echo '<div class="btn-container flex"><a href="/custom/cglcoll4saisons/App/controleurs/afficherdetaildepart.php?session=' . $depart->get("rowidDepart") . '">' . $langs->trans("btn-info") .' </a></div>';
                 echo "</article>";
-                } else {
-                    echo "<article class='card-liste-depart bagroudGrise'>";
-                        echo "<p>" . formatDateAffichage ($depart->get("dateDepart")) . "</p>";
-                        echo "<h3>" . $depart->get("intituleDepart") . "</h3>";
-                        // echo "<p>" . $depart->get("lieuDepart") . "</p>";
-                        echo '<div class="messageGrise">'. $langs->trans("messageContact") .'</div>';
-                        echo '<div class="messageGrise">'. $langs->trans("messageContact2") .'</div>';
-                    echo "</article>";
-                }
             }
+        }
     }
 
     if (!empty($listeLocations)) {
